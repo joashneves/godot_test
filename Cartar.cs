d7356f5d7a0dde0fe2a35249849670989d45c9ca
joashneves <joashneves@gmail.com>
2025-02-12 19:58:18 -0300

    seleciona

--- Cartar.cs
+++ Cartar.cs
@@ -4,8 +4,10 @@ using System;
 public partial class Cartar : Node2D
 {
 	private Texture2D _texture;
-	public Vector2 _PositionMouse1;
-	public Vector2 _PositionMouse2;
+	private bool _dragging = false;
+
+	private Vector2 _PositionMouse1;
+	private Vector2 _PositionMouse2;
 
 	[Export]
 	public Texture2D Texture{
@@ -19,26 +21,36 @@ public partial class Cartar : Node2D
 	}
 	public override void _Draw()
 	{
-		DrawLine( _PositionMouse2, _PositionMouse1, Colors.Green, 1.0f);
-		
+		DrawRect(new Rect2(_PositionMouse2[0],_PositionMouse2[1], _PositionMouse1[0] - _PositionMouse2[0],  _PositionMouse1[1] - _PositionMouse2[1]), Colors.Green, false);
 	}
 	public override void _Input(InputEvent @event)
 	{
-	// Mouse in viewport coordinates.
-	if (@event is InputEventMouseButton eventMouseButton)
+
+	if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left)
 	{
-		GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
-		_PositionMouse1 = GetViewport().GetMousePosition();
-
+		GD.Print("Mouse Click/Unclick at: ", mouseEvent.Position);
+		_PositionMouse2 = mouseEvent.Position;
+		if (!_dragging && mouseEvent.Pressed)
+				{
+					_dragging = true;
+				}
+			// Stop dragging if the button is released.
+			if (_dragging && !mouseEvent.Pressed)
+			{
+				_dragging = false;
+			}
 	}
-	else if (@event is InputEventMouseMotion eventMouseMotion){
-		GD.Print("Mouse Motion at: ", eventMouseMotion.Position);
-		_PositionMouse2 = GetViewport().GetMousePosition();
-		QueueRedraw();
-	}
+	else
+		{
+			if (@event is InputEventMouseMotion motionEvent && _dragging)
+			{
+				GD.Print("Mouse _dragging on: ", motionEvent.Position);
+				_PositionMouse1 = motionEvent.Position;
+				GD.Print(_PositionMouse2, _PositionMouse1);
+				QueueRedraw();
+			}
+		}
 
-	// Print the size of the viewport.
-	GD.Print("Viewport Resolution is: ", GetViewport().GetVisibleRect().Size);
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
